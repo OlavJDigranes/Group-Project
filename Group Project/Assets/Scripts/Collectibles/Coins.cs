74640b2aca04c9be603438b188e107ec6c671c37
Kamil Chwedura <kchwedura@gmail.com>
2022-04-23 17:55:29 +0100

    Added FloatingText
    Added the ability to display a floating text with an animation.
    The text floats up and display whatever is inputted into it.

--- Group Project/Assets/Scripts/Collectibles/Coins.cs
+++ Group Project/Assets/Scripts/Collectibles/Coins.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class Coins : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]private GameObject floatingTextPrefab;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        // Layer 3 is Player Layer
+        if(collision.gameObject.layer == 3)
+        {
+            Destroy(gameObject);
+            ShowText("+1");
+        }
+    }
+
+    void ShowText(string text)
+    {
+        if(floatingTextPrefab)
+        {
+            GameObject prefab = Instantiate(floatingTextPrefab, transform.position, Quaternion.identity);
+            prefab.GetComponentInChildren<TextMesh>().text = text;
+            prefab.GetComponentInChildren<TextMesh>().color = Color.yellow;
+            Destroy(prefab, 1);
+        }
+
     }
 }
